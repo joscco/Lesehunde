3690048a72bddfef273f348d756b38a3011a783f
Jonathan Schmitz <jomitz96@hotmail.com>
2024-01-14 00:01:47 +0100

    Add base href

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import {enableProdMode} from '@angular/core';
 import {environment} from './environments/environment';
 import {bootstrapApplication} from "@angular/platform-browser";
 import {AppComponent} from "./app/app.component";
-import {provideRouter, withHashLocation} from "@angular/router";
+import {provideRouter} from "@angular/router";
 import {MainPageComponent} from "./app/views/main-view/main-page.component";
 import {ImpressumComponent} from "./app/views/impressum-view/impressum-page.component";
 
@@ -17,6 +17,6 @@ bootstrapApplication(AppComponent, {
       {path: '', component: MainPageComponent},
       {path: 'impressum', component: ImpressumComponent},
       {path: '**', redirectTo: ''}
-    ], withHashLocation())
+    ])
   ]
 });
